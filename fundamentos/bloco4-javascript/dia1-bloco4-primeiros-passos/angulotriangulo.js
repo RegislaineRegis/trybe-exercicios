0d42fbafd1b68e3f272b18ae2e703eab5b347c82
Regislaine Regis <nanafidelis@gmail.com>
2021-11-28 20:09:32 -0300

    alteracao aquivo angulotriangulo, exercicio 11 salario liquido sobre IR e aliquotas InSS

--- fundamentos/bloco4-javascript/dia1-bloco4-primeiros-passos/angulotriangulo.js
+++ fundamentos/bloco4-javascript/dia1-bloco4-primeiros-passos/angulotriangulo.js
@@ -5,7 +5,7 @@ const angulo3 = 8;
 let resultadoTriangulo = angulo1 + angulo2 + angulo3;
 
 if(angulo1 > 0 && angulo2 > 0 && angulo3 > 0){
-    if(resultadoTriangulo === 180){
+    if(resultadoTriangulo == 180){
       console.log("Os ângulos representam os ângulos de um triângulo:",true);
 
      }else{
