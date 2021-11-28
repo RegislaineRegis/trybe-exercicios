fb0ee0e015fe2cc4a2f95a9c24d4bc9e60f1ee68
Regislaine Regis <nanafidelis@gmail.com>
2021-11-28 17:18:48 -0300

    impreme true caso um dos numeros sejam par, e false caso nenhum seje

---
+++ fundamentos/bloco4-javascript/dia1-bloco4-primeiros-passos/numeropar.js
@@ -0,0 +1,10 @@
+const numero1 = 8;
+const numero2 = 3;
+const numero3 = 4;
+
+
+if( (numero1%2 === 0) || (numero2%2 === 0)|| (numero3%2===0)){
+    console.log("Resposta:", true);
+}else{
+    console.log("Resposta:", false);
+}
