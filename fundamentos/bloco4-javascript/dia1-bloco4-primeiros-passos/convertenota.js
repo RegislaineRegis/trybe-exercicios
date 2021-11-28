d5f08cb3f3df77114a312ffa79b91c5cbd145722
Regislaine Regis <nanafidelis@gmail.com>
2021-11-28 17:17:11 -0300

    exercicio que coverte uma  nota dada em  porcentagem para letra em conceito (A a F)

---
+++ fundamentos/bloco4-javascript/dia1-bloco4-primeiros-passos/convertenota.js
@@ -0,0 +1,29 @@
+let notaPorcentagem = 100;
+
+    if( notaPorcentagem < 0 &&  notaPorcentagem < 100 ){  
+        console.log("Erro: Nota inválida");
+    }          
+      else if(notaPorcentagem >= 90){
+       console.log("Sua nota é: A");
+
+      } else if(notaPorcentagem >=80){  
+        console.log("Sua nota é: B");
+
+      }else if(notaPorcentagem >= 70){  
+         console.log(" Sua nota é: C");
+
+       }else if(notaPorcentagem >= 60){
+           console.log(" Sua nota é: D"); 
+
+        } else if(notaPorcentagem >= 50){
+            console.log(" Sua nota é: E");
+
+          } else {
+            console.log("Sua nota é: F");
+          }
+ 
+
+     
+ 
+     
+
