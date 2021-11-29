2545ae3a78dafcf233e5bfabd820ac6c1bcc82c6
Regislaine Regis <nanafidelis@gmail.com>
2021-11-28 22:00:46 -0300

    exercicios do conteudo arrays/for/ for-of

---
+++ fundamentos/bloco4-javascript/dia2-bloco4-array-loop-for/exercicio1-7.js
@@ -0,0 +1,41 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+/* imprimir todo o conteudo
+for(let index = 0; index < numbers.length; index +=1){
+
+    console.log (numbers[index]);
+}*/
+/* some todo o conteudo e imprima o resultado
+let soma = 0;
+for(let index = 0; index < numbers.length; index +=1){
+
+     soma = soma + numbers[index];
+}
+console.log("A soma é: ", soma);*/
+
+/*media aritmetica
+let soma=0;
+let media;
+for(let index = 0; index < numbers.length; index +=1){
+
+     soma = soma + numbers[index];
+}
+media = soma / (numbers.length);
+console.log("A media aritmetica é: ", media);*/
+
+/* caso a media maior que 20, imprima valor maior que 20, caso não valor menor que 20
+let soma=0;
+let media;
+for(let index = 0; index < numbers.length; index +=1){
+
+     soma = soma + numbers[index];
+}
+media = soma / (numbers.length);
+if(media > 20){
+console.log("Media é maior do que 20:", media);
+}else{
+    console.log("Media é menor do que 20:", media);
+}/*
+
+
