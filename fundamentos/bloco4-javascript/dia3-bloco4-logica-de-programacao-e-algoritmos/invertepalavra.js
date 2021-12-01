e74cc3833709c13e6bb430ca443f7902afffef88
Regislaine Regis <nanafidelis@gmail.com>
2021-12-01 14:18:07 -0300

    resoluçao do exercicio de palavra invertida

---
+++ fundamentos/bloco4-javascript/dia3-bloco4-logica-de-programacao-e-algoritmos/invertepalavra.js
@@ -0,0 +1,10 @@
+let word ='trybe';
+let wordAux ='';
+//console.log(word[word.length -1]);
+
+
+for(let index = 1; index <= word.length; index +=1){
+  wordAux += word[word.length - index];   
+
+}
+console.log(" a palavra :", word, "invertida: ", wordAux);
