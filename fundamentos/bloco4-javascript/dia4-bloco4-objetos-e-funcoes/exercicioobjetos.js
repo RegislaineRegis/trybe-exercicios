701451498b49b3eb4a243833bceb778c785cf3e9
Regislaine Regis <nanafidelis@gmail.com>
2021-12-02 08:50:27 -0300

    resoluçao do exercicio de fixaçao objeto

--- fundamentos/bloco4-javascript/dia4-bloco4-objetos-e-funcoes/exercicioobjetos.js
+++ fundamentos/bloco4-javascript/dia4-bloco4-objetos-e-funcoes/exercicioobjetos.js
@@ -6,3 +6,7 @@ medals:{ golden: 2,
          silver: 3, 
         },
 };
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+console.log('A Jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+console.log('A Jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo ' + player.bestInTheWorld.length +' vezes.');
+console.log('A Jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.' );
