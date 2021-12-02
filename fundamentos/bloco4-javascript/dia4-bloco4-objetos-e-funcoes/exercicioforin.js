f6991582b8f6cdde0cae99b4f5f81875348c7502
Regislaine Regis <nanafidelis@gmail.com>
2021-12-02 09:03:07 -0300

    exrecicio de fixaçao parte 2 forin

--- fundamentos/bloco4-javascript/dia4-bloco4-objetos-e-funcoes/exercicioforin.js
+++ fundamentos/bloco4-javascript/dia4-bloco4-objetos-e-funcoes/exercicioforin.js
@@ -1,8 +1,18 @@
-let names = {
+
+/*let names = {
     person1: 'João',
     person2: 'Maria',
     person3: 'Jorge' 
   };
    for(let nome in names){
       console.log('Olá ' + names[nome]);
-   }
+   }*/
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+for(let index in car){
+    console.log(index, car[index]);
+}
