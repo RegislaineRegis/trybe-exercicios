4797e48620a32bdf629bd0575dbd46093d2fbfe1
Regislaine Regis <nanafidelis@gmail.com>
2021-12-08 21:21:14 -0300

    fazendo os exemplos e teste do bloco 5 getElementById

---
+++ fundamentos/bloco5-javascript-dom-seletores/dia1-bloco5-dom-seletores/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Harry poter E a pedra filosofal";
+document.getElementById("second-paragraph").innerText = "Um dos filmes de maior venda de bilheteria, e de público a jovem e infantil.";
+document.getElementById("subtitle").innerText = "Filme Pedra filosofal bate record em bilheteria";
