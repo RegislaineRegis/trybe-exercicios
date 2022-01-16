ac19877907c3e94808dada73b321f996aec07d9f
RegislaineRegis <nanafidelis@gmail.com>
2022-01-16 18:44:37 -0300

    refatorando codigo fatorial

--- fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia1-javascriptES6-let-const-arrow-functions-e-template-literals/exercicios/fatorial.js
+++ fundamentos/bloco7-introducao-a-javascript-es6-e-testes-unitarios/dia1-javascriptES6-let-const-arrow-functions-e-template-literals/exercicios/fatorial.js
@@ -1,4 +1,4 @@
-const fatorial = (num) => {
+/*const fatorial = (num) => {
     let fator = 1;
     for(let index = 1; index <= num; index += 1){
      fator = fator * index;
@@ -6,4 +6,9 @@ const fatorial = (num) => {
     return fator; 
 }
 
-console.log(fatorial(5)); 
+console.log(fatorial(5)); */
+
+//refatorando 
+
+const fatorial = (num) => num > 1 ? num*fatorial(num-1) : 1;
+console.log(fatorial(4));
