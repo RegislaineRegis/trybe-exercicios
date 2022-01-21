32a0038631b45e60c46f34d90de4dc523dbb7b05
RegislaineRegis <nanafidelis@gmail.com>
2022-01-21 18:45:27 -0300

    criaçao da pasta e do exercicio1

---
+++ fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia3-map-filter/exercise1.js
@@ -0,0 +1,13 @@
+//Todos os exercícios devem ser realizados utilizando reduce , e se necessário outra HOF , a informação será citada no enunciado.
+//1 - Dada uma matriz, transforme em um array.
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+    
+
+  function flatten() {
+   return arrays.reduce((acumulador, currenteValue) => (acumulador+currenteValue), []);  
+  }
+console.log(flatten());
