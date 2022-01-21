7090e0fdb5119b3aaf14675636f66d6317278458
RegislaineRegis <nanafidelis@gmail.com>
2022-01-21 13:17:17 -0300

    criacao das pasta e da branch e resoluçao do primeiro exercicio do bloco8 dia 2

---
+++ fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia2-forEach-find-some-every-sort/exercise1.js
@@ -0,0 +1,71 @@
+//Estes exercícios praticam os conceitos de Higher Order Functions associados a outros já vistos, como arrow functions , template literals , objetos e temas dos fundamentos. Essa mistura de conceitos é muito importante para seu aprendizado, então use tudo o que sabe para resolver os exercícios! Utilize seguinte código como template para realizar os exercícios:
+
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+  
+  // 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947. Dica: use a função find .
+
+  function authorBornIn1947() {
+    return books.find((book) => book.author.birthYear === 1947).author.name;
+  }
+  console.log(authorBornIn1947());
