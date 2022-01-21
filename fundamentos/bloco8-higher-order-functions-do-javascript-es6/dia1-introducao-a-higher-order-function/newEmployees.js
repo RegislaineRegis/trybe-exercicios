aef07245ca2cb011608f354a80cd81c0ac208832
RegislaineRegis <nanafidelis@gmail.com>
2022-01-21 15:34:10 -0300

    realização do exercicio7

--- fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia1-introducao-a-higher-order-function/newEmployees.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia1-introducao-a-higher-order-function/newEmployees.js
@@ -1,8 +1,9 @@
-const cadastro = (nomeCompleto) => {
+//1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
+/*const cadastro = (nomeCompleto) => {
     const email = nomeCompleto.toLowerCase().split(' ').join('_');
     return {nomeCompleto, email:`${email}@trybe.com`};
 };
-
 
 const newEmployees = (callback) => {
     const employees = {
@@ -12,4 +13,20 @@ const newEmployees = (callback) => {
     }
     return employees;
   };
-  console.log(newEmployees(cadastro));
+  console.log(newEmployees(cadastro));*/
+  //2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+/*
+  const sorte = () => {
+    const vet = [ 1, 2, 3, 4, 5];
+    let numero = vet[Math.floor(Math.random() * 5) + 1];
+    return numero;    
+    };
+
+  const sorteio = (number, callback) => (number === callback ? 'Parabéns você ganhou': 'Tente novamente');
+
+console.log(sorteio(4,sorte()));*/
+
+//Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
