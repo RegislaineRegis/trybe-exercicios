63eb688e9e9375e7acfb4142d69a1efc65f9d6f2
RegislaineRegis <nanafidelis@gmail.com>
2022-01-24 20:34:18 -0300

    realizaçao do exercicio de fixaçao default destructuring

--- fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-spread-operator-parametro-rest-destructuring-e-mais/exercicio-fixar.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-spread-operator-parametro-rest-destructuring-e-mais/exercicio-fixar.js
@@ -74,3 +74,30 @@ const user = {
    console.log(numerosPares); // [6, 8, 10, 12];
 
 // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo*/
+/*
+const person = {
+    name: 'João',
+    lastName: 'Jr',
+    age: 34,
+  };
+  
+  const { nationality } = person;
+  console.log(nationality);*/
+
+  //Do jeito que está, quando passamos person para a função GetNationality o retorno é João is undefined . Ajuste a função GetNationality para que a chamada GetNationality(person) retorne João is Brazilian .
+/*
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));*/
