154e7a82990951c2c6a826f6312e22fcbd52bf28
RegislaineRegis <nanafidelis@gmail.com>
2022-01-24 20:19:17 -0300

    realizaçao de exercicios de fixacao array destructuring

--- fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-spread-operator-parametro-rest-destructuring-e-mais/exercicio-fixar.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-spread-operator-parametro-rest-destructuring-e-mais/exercicio-fixar.js
@@ -13,7 +13,7 @@ const fruitSalad = (fruit, additional) => {
 console.log(fruitSalad(specialFruit, additionalItens));*/
 
 //1 - Temos dois objetos, um com informações pessoais de uma pessoa usuária e outro com informações referentes ao cargo desta pessoa usuária na empresa trappistEnterprise . Precisamos criar um terceiro objeto, que terá os dados pessoais e os dados de cargo juntos. Para isso, utilize o spread operator .
-
+/*
 const user = {
     name: 'Maria',
     age: 21,
@@ -35,4 +35,42 @@ const user = {
 
   //2 - Com o objeto em mãos, imprima no console uma frase utilizando os dados do objeto criado anteriormente. Para isso, utilize a desestruturação de objetos em conjunto com template literals .
 
-  console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
+  console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);*/
+
+  //1 - Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
+  /*
+  const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+   //saudacoes[1](saudacoes[0]); // Olá
+
+    // Produza o mesmo resultado acima, porém utilizando array destructuring
+
+    const [saudacao, realizaSaudacao] = saudacoes;
+
+    console.log(realizaSaudacao(saudacao));*/
+
+    //2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis
+/*
+    let comida = 'gato';
+    let animal = 'água';
+    let bebida = 'arroz';
+
+    console.log(comida, animal, bebida); // arroz gato água*/
+
+  // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+  /*
+  const variado = ['gato', 'animal','arroz'];
+
+  const [comida,animal,bebida] = variado
+  console.log(comida, animal, bebida);*/
+
+  //3 - array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares .
+  /*
+  let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+  [,,, ...numerosPares] = numerosPares;
+
+
+   console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo*/
