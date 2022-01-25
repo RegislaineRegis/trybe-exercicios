a836eb8e2ff046dfacbecd9971a08acaa341ba10
RegislaineRegis <nanafidelis@gmail.com>
2022-01-24 21:29:25 -0300

    realizaçao do exercicio de fixacao default parameters

--- fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-spread-operator-parametro-rest-destructuring-e-mais/exercicio-fixar.js
+++ fundamentos/bloco8-higher-order-functions-do-javascript-es6/dia5-spread-operator-parametro-rest-destructuring-e-mais/exercicio-fixar.js
@@ -107,3 +107,11 @@ console.log(getNationality(person));*/
     longitude});
   
   console.log(getPosition(-19.8157, -43.9542));*/
+
+//Para praticar, escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.
+
+const multiply = (number, value = 1) => {
+    return number * value;
+  };
+  
+  console.log(multiply(8));
