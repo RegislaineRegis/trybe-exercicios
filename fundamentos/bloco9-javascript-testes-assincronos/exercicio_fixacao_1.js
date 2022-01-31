2f1621fb6b367a793458b95820f1b8b91ce29b76
RegislaineRegis <nanafidelis@gmail.com>
2022-01-31 08:09:58 -0300

    realizaçao do exercicio 1 de fixacacao bloco 9

---
+++ fundamentos/bloco9-javascript-testes-assincronos/exercicio_fixacao_1.js
@@ -0,0 +1,14 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return param(userToReturn);  
+};
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
