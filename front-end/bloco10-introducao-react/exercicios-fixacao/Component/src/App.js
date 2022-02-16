34c5627ccc2399ad7a2278a2185881c5672c2d07
RegislaineRegis <nanafidelis@gmail.com>
2022-02-16 15:59:02 -0300

    realizaçao dos arquivos necessarios para iniciar o bloco10, exercicio de fixacao primeiro component em React

---
+++ front-end/bloco10-introducao-react/exercicios-fixacao/Component/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Component from './Component';
+
+function App() {
+  return <Component/>;
+};
+
+export default App;
---
+++ front-end/bloco10-introducao-react/exercicios-fixacao/Component/src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return ( 
+    <div> 
+    <h1>Regislaine Regis</h1> 
+    <p>Tenho 37 anos, sou formada em Administração de empresas e estou amando estudar na Trybe.</p>
+    </div>);
+  }
+}
+
+export default Component;
