219935858474fa2b025d18ff0b9ad34ec7e29e5c
RegislaineRegis <nanafidelis@gmail.com>
2022-02-16 20:57:02 -0300

    realizaçao do exercicio2 - react bloco 10

---
+++ front-end/bloco10-introducao-react/dia1-introducao-a-react/exercicios-do-dia/exercise2-app/src/App.js
@@ -0,0 +1,15 @@
+import Content from './Content';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+   <Header />
+   <Content/>
+   <Footer/>
+   </div>
+  );
+}
+
+export default App;
