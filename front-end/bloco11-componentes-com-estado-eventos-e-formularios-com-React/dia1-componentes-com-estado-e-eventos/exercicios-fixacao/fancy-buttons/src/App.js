91e47ef3ab51134b6f152c98105ab62aeb67bc24
RegislaineRegis <nanafidelis@gmail.com>
2022-02-21 15:31:15 -0300

    alteracao para o exercicio de fixacao 2- insercao do construtor e do this e bind

--- front-end/bloco11-componentes-com-estado-eventos-e-formularios-com-React/dia1-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
+++ front-end/bloco11-componentes-com-estado-eventos-e-formularios-com-React/dia1-componentes-com-estado-e-eventos/exercicios-fixacao/fancy-buttons/src/App.js
@@ -5,26 +5,32 @@ import React from 'react';
 não é a maneira correta de se criar eventos
 em React! A função se refere ao componente,
 então deve ser parte de sua classe! */
-function handleClick1() {
-  console.log('Clicou no botão um!')
-}
-function handleClick2() {
-  console.log('Clicou no botão dois!')
-}
-function handleClick3() {
-  console.log('Clicou no botão três')
-}
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+  handleClick1() {
+    console.log('Clicou no botão um!')
+  }
+  handleClick2() {
+    console.log('Clicou no botão dois!')
+  }
+  handleClick3() {
+    console.log('Clicou no botão três')
+  }
   /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
   render() {
     return( 
     <div>   
-      <button onClick={handleClick1}>Meu botão - 1</button>
-      <button onClick={handleClick2}>Meu botão - 2</button>
-      <button onClick={handleClick3}>Meu botão - 3</button>    
+      <button onClick={this.handleClick1}>Meu botão - 1</button>
+      <button onClick={this.handleClick2}>Meu botão - 2</button>
+      <button onClick={this.handleClick3}>Meu botão - 3</button>    
     </div>
     )
   }
