8ce174fb9c0a1344332cfbeff83675a20410dd1a
Regislaine <nanafidelis@gmail.com>
2022-10-19 21:07:49 -0300

    feat: resolucao do exercicio 6

---
+++ Ciencia-da-Computacao/introducao-a-python/dia01-aprendendo-Python/exercicios/exercicio6.py
@@ -0,0 +1,14 @@
+def type_of_triangle(side1, side2, side3):
+    is_triangle = (
+            side1 + side2 > side3 and
+            side2 + side3 > side1 and
+            side1 + side3 > side2
+    )
+    if not is_triangle:
+        return "não é triângulo"
+    elif side1 == side2 == side3:
+        return "equilátero"
+    elif side1 == side2 or side2 == side3 or side1 == side3:
+        return "isósceles"
+    else:
+        return "escaleno"
